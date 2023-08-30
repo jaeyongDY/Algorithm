f40bf539fdea9689940317b935a4e40adbb112aa
JaeYongLee <jayong1109@naver.com>
2023-08-30 18:55:37 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+string result;
+void input() {
+    cin >> result;
+}
+
+void solved() {
+}
+void output() {
+    cout << result.size() <<endl;
+}
+
+int main(){
+    input();
+    solved();
+    output();
+    return 0;
+}
