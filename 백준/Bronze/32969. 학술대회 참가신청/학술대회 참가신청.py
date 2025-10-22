d6ffd9067d73f6065ef8c39009c7d03aacf6ff21
JaeYongLee <jayong1109@naver.com>
2025-10-21 22:21:42 -0700

    [Bronze III] Title: 학술대회 참가신청, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.py
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.py
@@ -1,16 +1,6 @@
-rally = {"digital humanities" : ["social", "history", "language", "literacy"], "public bigdata" : ["bigdata", "public", "society"]}
-
 n = input()
 
-n = n.split(" ")
-
-find = False
-
-for key,value in rally.items() :
-    for data in n :
-        if data in value :
-            find = True
-            print(key)
-            break
-    if find :
-        break
+if "bigdata" in n or "public" in n or "society" in n :
+    print("public bigdata")
+else :
+    print("digital humanities")
